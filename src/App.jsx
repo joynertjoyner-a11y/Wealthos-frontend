4ee42ab84a38dd3b902357baf8cf05a7a9d5fec7
joynertjoyner-a11y <joynertjoyner@gmail.com>
2026-04-23 04:05:54 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,8 +53,8 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/connect" element={<Connect />} />
-        <Route path="/analytics" element={<Analytics />} />
+<Route path="/connect" element={<Connect />} />
+<Route path="/analytics" element={<Analytics />} />
       </Routes>
     </Router>
   );
